b307f1310ab7f401fde283f330c5dc7dcadc803f
lab302-35 <lab302-35@labeng.local>
2019-01-11 16:34:51 +0700

    version 1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 int main()
-{
-    cout << "Hello";
+{   double x = 2 ,sum;
+    while(x<70){
+        sum +=1/x;
+        x=x+1;
+
+    }
+    cout << sum;
     return 0;
 }
